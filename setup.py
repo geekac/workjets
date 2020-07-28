ef7c9f23c19ae86f3343a3628851d01e4f1d3f19
geekac <geekac@163.com>
2020-07-28 18:19:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ module_czi = ext_module_czi()
 
 setuptools.setup(
     name="workjets",
-    version="0.1.4",
+    version="0.1.5",
     author="geekac",
     author_email="geekac@163.com",
     description="A common using tools library for work efficiently.",
